382583869048adbc04ffc9486389fa5372c97478
JiyaaG <143901490+JiyaaG@users.noreply.github.com>
2025-05-07 20:33:17 +0530

    fixed nav-menu transition

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -44,18 +44,10 @@ const Layout: React.FC<ChildrenProps> = ({ children }) => {
           <i className={`fas ${isMenuOpen ? 'fa-times' : 'fa-bars'}`}></i>
         </button>
         <nav className={`nav${isMenuOpen ? ' nav-open' : ''}${noTransition ? ' nav-no-transition' : ''}`}>
-          <Link href="/about" className={`nav-link ${isActive('/about') ? 'active-link' : ''}`} onClick={closeMenuInstant}>
-            ABOUT
-          </Link>
-          <Link href="/resume" className={`nav-link ${isActive('/resume') ? 'active-link' : ''}`} onClick={closeMenuInstant}>
-            RESUME
-          </Link>
-          <Link href="/projects" className={`nav-link ${isActive('/projects') ? 'active-link' : ''}`} onClick={closeMenuInstant}>
-            PROJECTS
-          </Link>
-          <Link href="/contact" className={`nav-link ${isActive('/contact') ? 'active-link' : ''}`} onClick={closeMenuInstant}>
-            CONTACT
-          </Link>
+          <Link href="/about" className={`nav-link ${isActive('/about') ? 'active-link' : ''}`} onClick={closeMenuInstant}>ABOUT</Link>
+          <Link href="/resume" className={`nav-link ${isActive('/resume') ? 'active-link' : ''}`} onClick={closeMenuInstant}>RESUME</Link>
+          <Link href="/projects" className={`nav-link ${isActive('/projects') ? 'active-link' : ''}`} onClick={closeMenuInstant}>PROJECTS</Link>
+          <Link href="/contact" className={`nav-link ${isActive('/contact') ? 'active-link' : ''}`} onClick={closeMenuInstant}>CONTACT</Link>
         </nav>
       </header>
       
